9d213c38d0f8f5c1f1146e35baf0c544fe1c60ce
nickolasbradham <nickolasbradham@gmail.com>
2022-12-14 21:46:07 -0700

    Preparing for new algorithm.

--- src/nbradham/satProdCalc/RecipeSearcher.java
+++ src/nbradham/satProdCalc/RecipeSearcher.java
@@ -31,57 +31,8 @@ final class RecipeSearcher {
 	 * @return The results of the search.
 	 */
 	CalcResults findBest() {
-		HashMap<String, ItemStack> recipeOuts;
-		HashMap<String, Double> reqsBuild = new HashMap<>(), byBuild = new HashMap<>();
-		ItemStack[] requirements = tracker.requirements();
-		String reqName, outName;
-		double multiplier = 0;
-
-		for (ItemStack requirement : requirements) {
-			for (Recipe recipe : RecipeManager.getRecipesByOutput(requirement.name())) {
-				if (recipe.isDisabled())
-					continue;
-
-				reqsBuild.clear();
-				byBuild.clear();
-
-				// Forward all requirements accept the one being processed.
-				for (ItemStack chk : requirements)
-					if (chk != requirement)
-						reqsBuild.put(chk.name(), chk.count());
-
-				// Forward all byproducts.
-				for (ItemStack add : tracker.byproducts())
-					byBuild.put(add.name(), add.count());
-
-				// Calculate recipe IO ratio at required rate.
-				multiplier = requirement.count()
-						/ (recipeOuts = recipe.getOutputs()).get(reqName = requirement.name()).count();
-
-				// TODO Check byproducts for reuse.
-
-				for (ItemStack out : recipeOuts.values())
-					if (!reqName.equals(outName = out.name()))
-						byBuild.put(outName, byBuild.getOrDefault(outName, 0d) + multiplier * out.count());
-
-				// TODO Process already computed recipes. Check if this resource pool is already
-				// found. Build and execute next RecipeSearcher. Check if results are better
-				// than current best. Etc.
-			}
-		}
+		//TODO New Version
 		return null;
-	}
-
-	/**
-	 * Checks if {@code chk} is a resource pool already found in this chain of
-	 * recipes.
-	 * 
-	 * @param chk The ResourceTracker to compare to previous trackers.
-	 * @return True if this RecipeSearcher or previous ones have the same resource
-	 *         pool.
-	 */
-	private boolean isPoolAlreadyFound(ResourceTracker chk) {
-		return chk.equals(tracker) || prev.isPoolAlreadyFound(chk);
 	}
 
 	/**
